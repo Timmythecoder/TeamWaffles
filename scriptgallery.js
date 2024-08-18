1287b84d2983f347b7c83908d20ec1739a642f23
Test 2 <98520323+Timmythecoder@users.noreply.github.com>
2024-08-18 21:06:10 +1000

    Add files via upload

--- scriptgallery.js
+++ scriptgallery.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const photos = [
         { src: "images/gallery/TWL O - Copy copy.png", description: "the good ol' Team Waffles Logo - thanks designer" },
+
+        { src: "images/gallery/playfightingthing.png", description: "Gee I don't condone this. I awarded 50 Points to Ben and Alex 18/08/24" },
+        { src: "images/gallery/playfightingthing1.png", description: "Gee I don't condone this. I awarded 50 Points to Ben and Alex 18/08/24" },
+
+        { src: "images/gallery/mm2.png", description: "MM2 with Tim, Max, Alex, Ben and Matthew 18/08/24" },
+        { src: "images/gallery/mm21.png", description: "MM2 with Tim, Max, Alex, Ben and Matthew 18/08/24" },
+
+        { src: "images/gallery/ftfsc1.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc2.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc3.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc4.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc5.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc6.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc7.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc8.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc9.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc10.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc11.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc12.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+        { src: "images/gallery/ftfsc13.png", description: "Flee The Facility with Max, Tim, Alex, Matthew, Ben, and Scarlett 18/08/24" },
+
+
+        
         { src: "images/gallery/ftf.png", description: "Flee The Facility with Max, Tim, and Alex 17/08/24" },
         { src: "images/gallery/ftf1.png", description: "Flee The Facility with Max, Tim, and Alex 17/08/24" },
         { src: "images/gallery/ftf2.png", description: "Flee The Facility with Max, Tim, and Alex 17/08/24" },
